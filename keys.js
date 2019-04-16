fb96300a52db015931a649100ef1388e93c5bdcb
aevers3 <aevers3@users.noreply.github.com>
2019-04-16 08:57:07 -0400

    file system fix - .env

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('Spotify keys loaded');
 
 exports.spotify = {
-  id: '24e09e17aa7d435baa99b2d0a2b89c1d',
-  secret: '7ad51d1ce5ef44aeb0eb05918f7a9e43'
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
